b2ba75bce9f56ee510d9b822bc0dd7afeaf4d537
João Victor Walcacer Giani <joaovictorgiani@gmail.com>
2026-02-08 17:24:42 -0300

    create project structure

---
+++ src/features.py
@@ -0,0 +1,45 @@
+VARIABLES = {
+    # DSS
+    "V0001": "uf",
+    "V0026": "situacao_domicilio",
+    "A001": "tipo_domicilio",
+    "A005012": "rede_agua",
+    "A00601": "agua_canalizada",
+    "A01901": "acesso_internet",
+
+    "C006": "sexo",
+    "C008": "idade",
+    "C009": "raca",
+
+    "D001": "alfabetizado",
+    "D00901": "escolaridade",
+
+    "I00102": "plano_saude",
+
+    "VDF002": "renda_domiciliar",
+    "VDF003": "renda_per_capita",
+
+    # Estilo de vida
+    "P00104": "peso",
+    "W00103": "peso_final",
+    "W00203": "altura",
+
+    "P050": "fumante_atual",
+    "P052": "fumou_passado",
+
+    "P02801": "alcool_semana",
+    "P035": "atividade_fisica",
+
+    # Targets
+    "Q00201": "hipertensao",
+    "Q03001": "diabetes",
+    "Q06306": "doenca_coracao",
+    "Q068": "avc",
+    "Q074": "asma",
+    "Q079": "artrite",
+    "Q088": "dort",
+    "Q092": "depressao",
+    "Q11605": "enfisema",
+    "Q11606": "bronquite",
+    "Q124": "insuficiencia_renal"
+}
